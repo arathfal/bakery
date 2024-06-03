27be5be2108d1da0ab70bb828e7eb3c03af07060
Aradea Atfal Risdianto <aradea.atfal@shipper.id>
2024-06-03 20:16:00 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Bakery',
+  description: 'A bakery offering a delightfu of freshly breads with the finest ingredients.'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
